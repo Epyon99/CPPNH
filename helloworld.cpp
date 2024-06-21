861237607f77fd7e77eb2cdc7aa2764835fbfbc4
Moises Chirinos <mfrchg@hotmail.com>
2024-06-21 16:18:28 -0500

    Lab1: Mejora en el codigo base

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,14 +6,11 @@ using namespace std;
 
 int main()
 {
-    string t;
-    std::cin >> t; 
-    std::cout << t;
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg = {"¡Hola", "en C++", "de", "from", "new", "Horizons!"};
     
-    for (const string& word : msg)
+    for (const string word : msg)
     {
         cout << word << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
